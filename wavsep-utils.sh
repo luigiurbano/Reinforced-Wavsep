7201630b983ed2f569b5f758b0d3f822fd4cbf5d
gx1 <18548727+giper45@users.noreply.github.com>
2024-04-20 16:05:16 +0200

    chore: add functions to wavsep utils

--- wavsep-utils.sh
+++ wavsep-utils.sh
@@ -6,8 +6,17 @@ function count_test_cases() {
     echo "Number of cases in ${PWD}: $COUNT"
 }
 
+function print_test_cases() {
+    find . -name \*.jsp | grep -v index.jsp 
+}
+
 function count_xss() {
     cd src/main/webapp/active/Reflected-XSS
     count_test_cases
+    cd ${BASEDIR}
+}
+function print_xss() {
+    cd src/main/webapp/active/Reflected-XSS
+    print_test_cases
     cd ${BASEDIR}
 }
